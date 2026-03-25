a8c119cdba58704158801aa40b598deb28ebabdc
Rupesh Kumar <rupeshkumar9312@gmail.com>
2026-03-25 21:25:42 +0530

    admin functionality completed

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -44,12 +44,6 @@ const Navigation = () => {
                 {item.name}
               </Link>
             ))}
-            <Link
-              to="/admin/login"
-              className="bg-yellow-500 hover:bg-yellow-600 text-primary px-4 py-2 rounded font-primary-bold transition-colors"
-            >
-              Admin
-            </Link>
           </div>
 
           {/* Mobile menu button */}
